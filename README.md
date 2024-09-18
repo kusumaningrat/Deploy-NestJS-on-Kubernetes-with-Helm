8957140aa1c4652ffc555cb8b2dffa356c504931
kusumaningrat <kusumanetcom@gmail.com>
2024-09-19 00:22:56 +0700

    Jenkins Pipeline

---
+++ README.md
@@ -0,0 +1,82 @@
+# Catch Up
+
+This is my example exercise about deploying NestJS application to Kubernetes Cluster with Helm.
+
+## Build Docker Image
+
+Navigate to `nest-api` directory and you will found the Dockerfile configuratio inside that can be use to build docker image for nestjs container. Let's build the docker image first.
+
+```bash
+docker build -t <your_dockerhub_username>/simple-nestjs-api:v1
+```
+Then, verify that the docker image successfully created
+```bash
+docker images
+```
+## Install NFS Storage
+
+Before going to deploy the database (PostgreSQL) and the NestJS application to Kubernetes, we need to setup the NFS first for storing our data.
+
+In your kubernetes master, do this:
+```
+# Install NFS package
+sudo apt-get update
+sudo apt-get install nfs-common nfs-kernel-server -y
+
+# Create NFS directory
+sudo mkdir -p /data/nfs
+sudo chown nobody:nogroup /data/nfs
+sudo chmod 2770 /data/nfs
+
+# Create NFS configuration
+echo -e "/data/nfs\t<your_ip>/24(rw,sync,no_subtree_check,no_root_squash)" | sudo tee -a /etc/exports
+
+# Test the configuration
+sudo exportfs -av
+
+# Restart and check the nfs service
+sudo systemctl restart nfs-kernel-server
+sudo systemctl status nfs-kernel-server
+```
+Then, in your kubernetes workers, do this:
+```bash
+sudo apt update
+sudo apt install nfs-common -y
+```
+
+## Deploy Database (PostgreSQL)
+
+Now, we will deploy the database first using helm chart. Navigate to `db` directory, then exec below command:
+```bash
+helm install postgres ./postgres
+```
+Verify that the instalation is successfull and running
+```bash
+helm list
+kubectl get all
+```
+Try to login to database using our credentials.
+```bash
+kubectl exec -it <postgres_pod_name> -- /bin/bash
+psql -U <your_username> -d <your_database>
+
+# Make sure you can successfully authenticated
+```
+
+## Deploy NestJS (API)
+
+Now, we will deploy the nestjs application using helm chart. Navigate to `bookshelf` directory, then exec below command:
+```bash
+helm install bookshelf ./bookshelf
+```
+Verify that the instalation is successfull and running
+```bash
+helm list
+kubectl get all
+```
+Now, see the logs of our deployments.
+```bash
+kubectl logs -f <bookshelf_pod_name>
+
+# Make sure that your service can connect to database
+```
---
+++ pipeline/bookshelf.groovy
@@ -0,0 +1,63 @@
+// Uses Declarative syntax to run commands inside a container.
+pipeline {
+    agent any
+    options{
+        disableConcurrentBuilds()
+        buildDiscarder logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '15')
+    }
+    environment{
+        NAMESPACE = "default"
+        IMAGE = "kusumaningrat16/simple-nestjs-api"
+        FAILED_STAGE=''
+    }
+    stages {
+        stage('Pull Repo') {
+            steps {
+                script{
+                    timeout(time: 30){
+                        FAILED_STAGE='Pull Repo'
+                        git branch: 'dev', credentialsId: 'github', url: 'git@github.com:kusumaningrat/kusumaningrat-Deploy-NestJS-on-Kubernetes-with-Helm.git'
+                    }
+                }
+            }
+        }
+        stage('Build'){
+            steps{
+                script{
+                    timeout(time:30){
+                        FAILED_STAGE='Build'
+                        withCredentials([usernamePassword(credentialsId: 'docker_hub')]){
+                            sh "docker build -t ${IMAGE}:${params.VERSION} ."
+                            sh "docker push ${IMAGE}:${params.VERSION}"
+                        }
+                    }
+                
+                }
+            }
+        }
+        stage('Clone Helm Repo'){
+            steps{
+                script{
+                    timeout(time: 30){
+                        FAILED_STAGE= 'Helm'
+                        git branch: 'helm', credentialsId: 'github', url: 'https://git-rbi.jatismobile.com/devops/bento-helm/admin-fe.git'
+                    }
+                }
+            }
+        }
+        stage('Deploy'){
+            steps{
+                script{
+                    timeout(time: 30){
+                        FAILED_STAGE='Deploy'
+                        withKubeConfig([credentialsId: 'fba1e179-6e2b-4c53-a2e6-4855f795748f']){
+                            sh "helm upgrade --install --set image.tag=${params.VERSION}-${env.ENV} --atomic --wait ${env.SERVICE} helm -n ${env.NAMESPACE}"
+                            sh "kubectl rollout status deployment ${env.SERVICE} -n ${env.NAMESPACE}"
+                        }
+                    } 
+                }
+            }
+        }
+       
+    }
+}
